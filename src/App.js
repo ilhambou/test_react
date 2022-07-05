9655b0e5c24f4d077b9999c031f0699802280a8e
ilhambou <bouichilham2@gmail.com>
2022-07-05 14:53:56 +0200

    test_react_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from "react"
+import AddTask from './components/AddTask';
 
-function App() {
+
+
+// import React from 'react';
+
+const App = () => {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'Dox',
+            day: 'feb',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Dox2',
+            day: 'feb2',
+            reminder: true,
+        },
+        {
+          id: 3,
+          text: 'Dox3',
+          day: 'feb23',
+          reminder: false,
+      },
+    ]
+)
+
+const addTask=(task)=>{
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks,newTask])
+}
+  
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task)=>task.id !== id))
+}
+
+const toggleRemider = (id) => {
+  setTasks(tasks.map((task)=>task.id === id ? { ...task,reminder: !task.reminder} : task))
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title={'Test'} />
+      <AddTask onAdd={addTask}/>
+      {tasks.length > 0 ? (
+      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleRemider}/>) : ('No Tasks to show')
+      }
     </div>
-  );
+      
+  )
 }
 
 export default App;
+// class App extends React.Component{
+//   render(){
+//     return <h1>hhh</h1>
+//   }
+// }
+// export default App;
